5f111d8129ed2d8dfb5b28bb9aa6fa7eaa542770
Eric <ericpaulin78055@gmail.com>
2022-05-17 16:03:41 -0700

    Added Form + Popup ope and close functionality

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ book.prototype.info = function(title, author, pageCount) {
 const book1 = new book("A Song of Ice and Fire", "George R.R Martin", 1400, false);
 //book1.info();     */
 
+
 /* Post ES6 "syntactical sugar" classes */
 
 class book {
@@ -25,12 +26,9 @@ class book {
         this.read = read;
     }
     info(title, author, pageCount) {
-        console.log(`${this.title} is by author ${this.author} and has a page count of ${this.pageCount}`)
+        console.log(`${title} is by author ${author} and has ${pageCount} pgs`)
     }
 }
-
-const ASOIAF = new book('A Song of Ice and Fire', 'George R.R Martin', 1400, true);
-ASOIAF.info();
 
 /* Array storing each book + valriable for temp storing current new book */
 let myLibrary = [];
@@ -39,6 +37,42 @@ let addBook;
 
 /* Pushes new Book titles to the end of the array */
 function addBookToLibrary() {
+    popUp.style.display = 'none';
     newBook = new book(title, author, pageCount, read);
     myLibrary.push(newBook);
+    bookCard();
+    form.reset();
 }
+
+
+/* Creates Book Card Visual */
+function bookCard() {
+    const display = document.getElementById('full-library');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+
+    for (var i = 0; i < myLibrary.length; i++){
+        createCard(myLibrary[i]);
+    }
+}
+
+
+/* Opens and Closes the Popup */
+
+function openPopup() {
+    document.getElementById("popUp").style.display = "grid";
+}
+
+function closePopup() {
+    document.getElementById("popUp").style.display = "none";
+}
+
+window.onclick = function (event) {
+    let modal = document.getElementById('popUp');
+    if (event.target == modal) {
+      closePopup();
+    }
+}
+
+document.getElementById("add").onclick = openPopup;
+document.getElementById("window").onclick = closePopup;
