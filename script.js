752bb410b60da16bd5599971602bee22072d5e41
Eric <ericpaulin78055@gmail.com>
2022-05-18 22:30:41 -0700

    js for form and cards

--- script.js
+++ script.js
@@ -27,33 +27,115 @@ class book {
         this.pageCount = pageCount;
         this.read = read;
     }
-    info(title, author, pageCount) {
-        console.log(`${title} is by author ${author} and has ${pageCount} pgs`)
-    }
 }
 
 /* Array storing each book + valriable for temp storing current new book */
 let myLibrary = [];
-let addBook;
 
+/* Add Book to Library (pushes it to end of array) */
+const bookInput = document.querySelectorAll('.book-input');
 
-/* Pushes new Book titles to the end of the array */
 function addBookToLibrary() {
-    popUp.style.display = 'none';
-    newBook = new book(title, author, pageCount, read);
-    myLibrary.push(newBook);
-    bookCard();
-    form.reset();
+    const book = new Book (
+        bookInput[0].value,
+        bookInput[1].value,
+        bookInput[2].value,
+        bookInput[3].checked,)
+        myLibrary.push(book)
 }
 
+const books = document.querySelector('.books')
+const addBook = document.querySelector('.add-book')
+const plusBtn = document.querySelector('.plusBtn')  
+const addBookBtn = document.querySelector('.add-book-btn')
 
-/* Creates Book Card Visual */
-function bookCard() {
-    const display = document.getElementById('book-list');
-    const books = document.querySelectorAll('.book');
-    books.forEach(book => display.removeChild(book));
+/* Form (Adding and Printing) */
 
-    for (var i = 0; i < myLibrary.length; i++){
-        createCard(myLibrary[i]);
+plusBtn.addEventListener('click', () => {
+    for (i = 0; i < 3; i++) {
+        bookInput[i].value = ''
     }
+    addBook.style.display = ('grid');
+})
+
+addBookBtn.addEventListener('click', () => {
+    for (i = 0; i < 3; i++) {
+        if (!bookInput[i].value) return
+    }
+    addBookToLibrary();
+    printLibrary();
+   // addBook.style.display = 'none';
+})
+
+function printLibrary() {
+    const booksListInitial = document.querySelector('.container');
+    books.removeChild(booksListInitial);
+
+    const booksListAfter = document.createElement('div');
+    booksListAfter.classList.add('.container');
+
+    for (let unit of myLibrary) {
+        const card = document.createElement('div');
+        const bookTitle = document.createElement('div');
+        const bookAuthor = document.createElement('div');
+        const bookPageCount = document.createElement('div');
+        const readBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+
+        card.classList.add('card');
+        bookTitle.classList.add('bookTitle');
+        bookAuthor.classList.add('bookAuthor');
+        bookPageCount.classList.add('bookPageCount');
+        readBtn.classList.add('readBtn');
+        deleteBtn.classList.add('deleteBtn');
+
+        bookTitle.textContent = unit.title;
+        bookAuthor.textContent = unit.author;
+        bookPageCount.textContent = unit.pageCount + ' pages';
+        let bookStatus = 'Not Read';
+        readBtn.style.backgroundColor = '#822634';
+        if (unit.bookStatus) {
+            bookStatus = 'Read';
+            readBtn.style.backgroundColor = 'black';
+        }
+        readBtn.textContent = bookStatus;
+        deleteBtn.textContent = 'Delete';
+
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPageCount);
+        card.appendChild(readBtn);
+        card.appendChild(deleteBtn);
+        console.log(unit)
+    }
+
+    books.appendChild(booksListAfter);
+    readBtnEvents();
+    deleteBtnEvents();
+}
+
+printLibrary();
+
+/* Functions for Read + Delete */
+
+function readBtnEvents() {
+    const readBtns = document.querySelectorAll('.readBtn');
+    let readBtnArray = Array.from(readBtns)
+
+    readBtnArray.forEach((button) => {
+        button.addEventListener('click', () => {
+            myLibrary[readBtnArray.indexOf(button)].read = !(myLibrary[readBtnArray.indexOf(button).read])
+            printLibrary();
+        });
+    });
+}
+
+function deleteBtnEvents() {
+    const deleteBtns = document.querySelectorAll('.deleteBtn');
+    let deleteBtnsArray = Array.from(deleteBtns);
+    deleteBtnsArray.forEach((button) => {
+        button.addEventListener('click', () => {
+            myLibrary.splice(deleteBtnsArray.indexOf(button), 1);
+        });
+    });
 }
